988c5d00f075304eb8ee2e3ece3e677d04d58663
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-10-16 22:19:24 -0400

    created a new folder to house my faker mock data

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-
 const transactions = require("./controllers/transactionsController");
+
+const seedDatabase = require("./seed/seed")
+seedDatabase();
 
 module.exports = app
