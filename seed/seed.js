e76bc484d4012dfc53ddb48a8d13cc1179e9c15c
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-10-16 23:50:46 -0400

    fixing code logic in seedjs

--- seed/seed.js
+++ seed/seed.js
@@ -12,23 +12,27 @@ db.serialize(() => {
         category TEXT
     )`);
 
-
-const numberOfRecords = 20;
+    const numberOfRecords = 20;
 
     const stmt = db.prepare(`
         INSERT INTO transactions (item_name, amount, date, from_who, category) VALUES (?, ?, ?, ?, ?)`);
-    
 
-    for(let i = 0; i < numberOfRecords; i++ ){
+    for (let i = 0; i < numberOfRecords; i++) {
         const item_name = faker.commerce.productName();
         const amount = faker.finance.amount();
         const date = faker.date.recent();
-        const from_who =faker.company.companyName();
+        const from_who = faker.company.companyName();
         const category = faker.commerce.department();
 
         stmt.run(item_name, amount, date, from_who, category);
     }
-    stmt.finalize
+    stmt.finalize(() => {
+        db.close((err) => {
+            if (err) {
+                console.error('Error closing the database:', err.message);
+            } else {
+                console.log('Database closed.');
+            }
+        });
+    });
 });
-
-db.close()
