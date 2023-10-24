f0abdbf8b436b56f3482839d786184fb3391f219
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-10-24 02:59:17 -0400

    completed with edits 404 page mounted

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const transactions = require('./controllers/transactionsController');
  
 app.use(express.json());
 
+app.use((req, res) => {
+  res.status(404).send('<h1> "404_Not_Found"</h1>');
+});
 
 app.use("/transactions", transactions);
 
-app.use((req, res) => {
-  // console.log('Catch-All Route Reached');
-  res.status(404).send('<h1> "404_Not_Found"</h1>');
-});
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const app = require("./app")
 
-
 require("dotenv").config();
-const PORT= process.env.PORT || 3333;
 
+const PORT= process.env.PORT || 3050;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
